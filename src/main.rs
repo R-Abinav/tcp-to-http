17487650ddf435ef83829d3d3d8e25faefaac975
R-Abinav <r.abinav1712@gmail.com>
2026-05-27 18:11:51 +0530

    chore: returning HTML

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs,
     net::{TcpListener, TcpStream},
     io::{BufReader, prelude::*}
 };
@@ -36,11 +37,18 @@ fn handle_connection(mut stream: TcpStream) -> std::io::Result<()>{
     println!("request: {http_request:#?}");   
 
     //structure the response according the HTTP response structure 
-    let body = "Hello there big boss!";
-    let response = format!("HTTP/1.1 200 OK\r\n\
-                                    Content-Length: {}\r\n\
-                                    \r\n\
-                                    {}", body.len(), body);
+    // let body = "Hello there big boss!";
+    // let response = format!("HTTP/1.1 200 OK\r\n\
+    //                                 Content-Length: {}\r\n\
+    //                                 \r\n\
+    //                                 {}", body.len(), body);
+
+    //returning HTML
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("example.html")?;
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes())?;
     Ok(())
