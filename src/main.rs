f2a41a2152449527dc4a22cf062932cabf10d69f
R-Abinav <r.abinav1712@gmail.com>
2026-05-27 16:36:36 +0530

    fix: idiomatic style of code

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
 use std::net::TcpListener;
 
-fn main() {
-    //tcp listener
-    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+fn main() -> std::io::Result<()>{
+    //tcp listener (idiomatic style)
+    let listener = TcpListener::bind("127.0.0.1:8080")?;
 
     for stream in listener.incoming(){
-        let stream = stream.unwrap();
+        match stream {
+            Ok(_stream) => {
+                println!("connection established!");
+            },
+            Err(err) => {
+                eprintln!("could not establish connection. Reason: {}", err);
+                continue;
+            }
+        };
 
         println!("connection established!");
     }
+
+    Ok(())
 }
